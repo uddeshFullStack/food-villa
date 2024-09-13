730b93501d969819e0b97482ef10471956cf5ad0
uddesh-kr-bhagat <136488707+uddesh-kr-bhagat@users.noreply.github.com>
2024-09-13 16:30:49 +0530

    "UI correction part 1"

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,45 @@
 import React, { useContext } from "react";
 import { Shimmer } from "./Shimmer";
-
 import { useState, useEffect } from "react";
 
 import useOnline from "../utils/online";
 import UserContext from "../utils/UserContext";
 import { Link } from "react-router-dom";
+import { MdOutlineStar } from "react-icons/md";
 
-const RestaurantCard=({name,cuisines,cloudinaryImageId,avgRating})=>{
-    const {user, setUser}=useContext(UserContext)
-    return(
-      <div className="w-[200px] p-2 m-2 shadow-md bg-pink-50">
-        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/"+cloudinaryImageId}></img>
-        <h2 className="font-bold text-2xl">{name}</h2>
-        <h3>{cuisines.join(", ")}</h3>
-        <h4 className="font-bold text-xl">{avgRating} stars</h4>
-        <h4>{user.name}</h4>
-        <h4>{user.email}</h4>
+const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating }) => {
+  const { user } = useContext(UserContext);
+
+  return (
+    <div className="w-[200px] p-2 m-2 bg-white">
+      <div className="relative">
+        <img
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/" +
+            cloudinaryImageId
+          }
+          alt={name}
+          className="w-full rounded-xl h-[173px]"
+        />
+        <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white font-bold px-2  rounded-b-xl">
+          <h2 className="text-[18px] truncate">items 60 % off</h2>
+        </div>
       </div>
-    );
-  };
-  
+      
+      <h2 className="font-bold text-[18px] truncate">{name}</h2>
+      <h3 className="truncate">{cuisines.map((cuisine) => cuisine).join(", ")}</h3>
+      <div className="flex items-center space-x-1">
+        <div className="bg-green-600 p-1 rounded-full inline-block">
+          <MdOutlineStar className="text-white"/>
+        </div>
+        <p className="font-bold text-xl">{avgRating.toString()} stars</p>
+      </div>
+      <h4 className="truncate">{user.name}</h4>
+      <h4 className="truncate">{user.email}</h4>
+    </div>
+  );
+};
+
   
   function filterData(searchText,restaurants){
       const data= restaurants.filter((restaurant) => 
@@ -59,7 +78,7 @@ export const Body=()=>{
         <h1>"sorry you are offline"</h1>
       )
     }
-    return allrestaurants.length===0?(<> <Shimmer /> </> ):(
+    return allrestaurants?.length===0?(<> <Shimmer /> </> ):(
       <>
       <div className="search-container p-5 bg-pink-50 my-2">
         <input type="text" 
@@ -81,7 +100,7 @@ export const Body=()=>{
         Search</button>
       </div>
       
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap mx-24 gap-x-5 justify-center items-center rounded-md bg-white">
         {
           filteredRestaurants.map(restaurant => {
             return( 
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { Shimmer } from './Shimmer';
 import { restaurantMenuList } from '../../contant';
 import { addItem } from '../utils/cartSlice';
 import { useDispatch } from 'react-redux';
+import { MdOutlineStar } from "react-icons/md";
+import UserContext from "../utils/UserContext";
 
 export const RestaurantMenu = () => {
+  const { user } = useContext(UserContext);
   const resId  = useParams();
   const [data, setData] = useState(null);
 
@@ -39,29 +42,55 @@ export const RestaurantMenu = () => {
   const restaurants = data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
 
   return (
-    <div className='flex'>
+    <div className='flex m-5'>
       <div>
         {restaurants
           .filter(restaurant => restaurant.info.id === resId.id)
           .map(restaurant => (
-            <div className="w-[200px] p-2 m-2 shadow-md bg-pink-50">
-              <h1 className='font-mono font-bold text-3xl'>{restaurant.info.name}</h1>
-              <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/"+restaurant.info.cloudinaryImageId}></img>
-              <h3>{restaurant.info.cuisines.join(", ")}</h3>
-              <h4 className="font-bold text-xl">{restaurant.info.avgRating} stars</h4>
-            </div>
+            <div className="w-[200px] p-2 m-2 bg-white">
+              <div className="relative">
+                <img
+                  src={
+                    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/" +
+                    restaurant.info.cloudinaryImageId
+                  }
+                  alt={name}
+                  className="w-full rounded-xl h-[173px]"
+                />
+                <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white font-bold px-2  rounded-b-xl">
+                  <h2 className="text-[18px] truncate">items 60 % off</h2>
+                </div>
+              </div>
+            
+            <h2 className="font-bold text-[18px] truncate">{restaurant.info.name}</h2>
+            <div className="flex items-center space-x-1">
+              <div className="bg-green-600 p-1 rounded-full inline-block">
+                <MdOutlineStar className="text-white"/>
+              </div>
+              <p className="font-bold text-xl">{restaurant.info.avgRating.toString()} stars</p>
+            </div>  
+            <h4 className="truncate">{user.name}</h4>
+            <h4 className="truncate">{user.email}</h4>
+          </div>
+      
           ))
         }
       </div>
-      <div>
-      <div className='font-mono font-bold text-3xl text-center  bg-pink-50'>Menus</div>
-      <div className='flex flex-wrap justify-center'>
+      <div className='flex flex-col justify-start items-center mx-10 my-5'>
+      <div className='font-mono font-bold text-3xl text-center mb-5'>Menus</div>
+      <div className='flex flex-col justify-center'>
         {restaurantMenuList.menus.map(menu => (
-          <div key={menu.category} className="p-2 m-2 shadow-md bg-pink-50">
-            <h2 className="font-bold text-xl">{menu.category}</h2>
+        <div className='flex flex-row'>
+          <div key={menu.category}>
+            <h2 className="font-bold w-[100px] text-xl">{menu.category}</h2>
+          </div>
+          <div key={menu.category} className="p-2 m-2 flex flex-wrap ">
+            
             {menu.items.map(item => (
-              <div key={item.name} className="w-[180px] p-2 m-2 shadow-md bg-pink-50">
-                {/* <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/"+restaurant.info.cloudinaryImageId}></img> */}
+              <div key={item.name} className="w-[180px]  p-2 m-2 ">
+                <img src={"https://www.anifabiriyani.com/meta/biriyani-spl.webp"} 
+                className='w-full h-[150px] rounded-md '
+                ></img>
                 <h2 className="font-bold text-xl">{item.name}</h2>
                 <h3>{item.description}</h3>
                 <h4 className="font-bold text-xl">{item.price} $</h4>
@@ -69,14 +98,14 @@ export const RestaurantMenu = () => {
                   onClick={()=>{addFoodItems(item)}}
                 >
                 Add</button>
-                </div>
+              </div>
             ))}
           </div>
+        </div>
         ))}
       </div>
       </div>
     </div>
-    
   );
 };
 
